a9da1e0c079b918ee9162302934b9b2225583e8a
Sean Reifschneider <jafo@tummy.com>
2009-04-02 12:48:19 -0600

    Fixing arguments passed to the _Error if invalid read length.

--- memcache.py
+++ memcache.py
@@ -924,7 +924,7 @@ class _Host:
             buf += foo
             if len(foo) == 0:
                 raise _Error, ( 'Read %d bytes, expecting %d, '
-                        'read returned 0 length bytes' % ( len(buf), foo ))
+                        'read returned 0 length bytes' % ( len(buf), rlen ))
         self.buffer = buf[rlen:]
         return buf[:rlen]
 
