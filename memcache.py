4d2444aeaf1b3c05052f72c44ae0740ce9a34d40
Sean Reifschneider <jafo@guin.tummy.com>
2009-11-27 18:06:49 -0700

    * incr/decr were raising ValueError if the key did not exist, the      docstring said it returned none.  Patch by Chihiro Sakatoku.
       * Included cmemcache_hash, by permission of Ludvig Ericson.
         To use, do "memcached.cmemcache_activate()".
    
       * Adding cas method, submitted by Ben Gutierrez.

--- memcache.py
+++ memcache.py
@@ -157,6 +157,7 @@ class Client(local):
         self.set_servers(servers)
         self.debug = debug
         self.stats = {}
+        self.cas_ids = {}
 
         # Allow users to modify pickling/unpickling behavior
         self.pickleProtocol = pickleProtocol
@@ -380,12 +381,13 @@ class Client(local):
 
     def incr(self, key, delta=1):
         """
-        Sends a command to the server to atomically increment the value for C{key} by
-        C{delta}, or by 1 if C{delta} is unspecified.  Returns None if C{key} doesn't
-        exist on server, otherwise it returns the new value after incrementing.
+        Sends a command to the server to atomically increment the value
+        for C{key} by C{delta}, or by 1 if C{delta} is unspecified.
+        Returns None if C{key} doesn't exist on server, otherwise it
+        returns the new value after incrementing.
 
-        Note that the value for C{key} must already exist in the memcache, and it
-        must be the string representation of an integer.
+        Note that the value for C{key} must already exist in the memcache,
+        and it must be the string representation of an integer.
 
         >>> mc.set("counter", "20")  # returns 1, indicating success
         1
@@ -425,6 +427,7 @@ class Client(local):
         try:
             server.send_cmd(cmd)
             line = server.readline()
+            if line.strip() =='NOT_FOUND': return None
             return int(line)
         except socket.error, msg:
             if type(msg) is types.TupleType: msg = msg[1]
@@ -500,6 +503,36 @@ class Client(local):
         indicating don't ever try to compress.
         '''
         return self._set("set", key, val, time, min_compress_len)
+
+
+    def cas(self, key, val, time=0, min_compress_len=0):
+        '''Sets a key to a given value in the memcache if it hasn't been
+        altered since last fetched. (See L{gets}).
+
+        The C{key} can optionally be an tuple, with the first element
+        being the server hash value and the second being the key.
+        If you want to avoid making this module calculate a hash value.
+        You may prefer, for example, to keep all of a given user's objects
+        on the same memcache server, so you could use the user's unique
+        id as the hash value.
+
+        @return: Nonzero on success.
+        @rtype: int
+        @param time: Tells memcached the time which this value should expire,
+        either as a delta number of seconds, or an absolute unix
+        time-since-the-epoch value. See the memcached protocol docs section
+        "Storage Commands" for more info on <exptime>. We default to
+        0 == cache forever.
+        @param min_compress_len: The threshold length to kick in
+        auto-compression of the value using the zlib.compress() routine. If
+        the value being cached is a string, then the length of the string is
+        measured, else if the value is an object, then the length of the
+        pickle result is measured. If the resulting attempt at compression
+        yeilds a larger string than the input, then it is discarded. For
+        backwards compatability, this parameter defaults to 0, indicating
+        don't ever try to compress.
+        '''
+        return self._set("cas", key, val, time, min_compress_len)
 
 
     def _map_and_prefix_keys(self, key_iterable, key_prefix):
@@ -681,7 +714,16 @@ class Client(local):
         store_info = self._val_to_store_info(val, min_compress_len)
         if not store_info: return(0)
 
-        fullcmd = "%s %s %d %d %d\r\n%s" % (cmd, key, store_info[0], time, store_info[1], store_info[2])
+        if cmd == 'cas':
+            if key not in self.cas_ids:
+                return self._set('set', key, val, time, min_compress_len)
+            fullcmd = "%s %s %d %d %d %d\r\n%s" % (
+                    cmd, key, store_info[0], time, store_info[1],
+                    self.cas_ids[key], store_info[2])
+        else:
+            fullcmd = "%s %s %d %d %d\r\n%s" % (
+                    cmd, key, store_info[0], time, store_info[1], store_info[2])
+
         try:
             server.send_cmd(fullcmd)
             return(server.expect("STORED") == "STORED")
@@ -690,21 +732,24 @@ class Client(local):
             server.mark_dead(msg)
         return 0
 
-    def get(self, key):
-        '''Retrieves a key from the memcache.
-
-        @return: The value or None.
-        '''
+    def _get(self, cmd, key):
         check_key(key)
         server, key = self._get_server(key)
         if not server:
             return None
 
-        self._statlog('get')
+        self._statlog(cmd)
 
         try:
-            server.send_cmd("get %s" % key)
-            rkey, flags, rlen, = self._expectvalue(server)
+            server.send_cmd("%s %s" % (cmd, key))
+            rkey = flags = rlen = cas_id = None
+            if cmd == 'gets':
+                rkey, flags, rlen, cas_id, = self._expect_cas_value(server)
+                if rkey:
+                    self.cas_ids[rkey] = cas_id
+            else:
+                rkey, flags, rlen, = self._expectvalue(server)
+
             if not rkey:
                 return None
             value = self._recv_value(server, flags, rlen)
@@ -714,6 +759,20 @@ class Client(local):
             server.mark_dead(msg)
             return None
         return value
+
+    def get(self, key):
+        '''Retrieves a key from the memcache.
+
+        @return: The value or None.
+        '''
+        return self._get('get', key)
+
+    def gets(self, key):
+        '''Retrieves a key from the memcache. Used in conjunction with 'cas'.
+
+        @return: The value or None.
+        '''
+        return self._get('gets', key)
 
     def get_multi(self, keys, key_prefix=''):
         '''
@@ -786,6 +845,16 @@ class Client(local):
                 if type(msg) is types.TupleType: msg = msg[1]
                 server.mark_dead(msg)
         return retvals
+
+    def _expect_cas_value(self, server, line=None):
+        if not line:
+            line = server.readline()
+
+        if line[:5] == 'VALUE':
+            resp, rkey, flags, len, cas_id = line.split()
+            return (rkey, int(flags), int(len), int(cas_id))
+        else:
+            return (None, None, None, None)
 
     def _expectvalue(self, server, line=None):
         if not line:
@@ -1146,5 +1215,118 @@ if __name__ == "__main__":
             print "FAIL"
         else:
           print "OK"
+
+
+########################################################
+"""cmemcache-style CRC32 "hashing" for python-memcached.
+
+Imported from http://pypi.python.org/pypi/cmemcache_hash
+by permission of Ludvig Ericson
+"""
+
+crc32tab = (
+  0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,
+  0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
+  0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
+  0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
+  0x1db71064, 0x6ab020f2, 0xf3b97148, 0x84be41de,
+  0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
+  0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec,
+  0x14015c4f, 0x63066cd9, 0xfa0f3d63, 0x8d080df5,
+  0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
+  0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b,
+  0x35b5a8fa, 0x42b2986c, 0xdbbbc9d6, 0xacbcf940,
+  0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
+  0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116,
+  0x21b4f4b5, 0x56b3c423, 0xcfba9599, 0xb8bda50f,
+  0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
+  0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d,
+  0x76dc4190, 0x01db7106, 0x98d220bc, 0xefd5102a,
+  0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
+  0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818,
+  0x7f6a0dbb, 0x086d3d2d, 0x91646c97, 0xe6635c01,
+  0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
+  0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457,
+  0x65b0d9c6, 0x12b7e950, 0x8bbeb8ea, 0xfcb9887c,
+  0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
+  0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2,
+  0x4adfa541, 0x3dd895d7, 0xa4d1c46d, 0xd3d6f4fb,
+  0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
+  0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9,
+  0x5005713c, 0x270241aa, 0xbe0b1010, 0xc90c2086,
+  0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
+  0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4,
+  0x59b33d17, 0x2eb40d81, 0xb7bd5c3b, 0xc0ba6cad,
+  0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
+  0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683,
+  0xe3630b12, 0x94643b84, 0x0d6d6a3e, 0x7a6a5aa8,
+  0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
+  0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe,
+  0xf762575d, 0x806567cb, 0x196c3671, 0x6e6b06e7,
+  0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
+  0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5,
+  0xd6d6a3e8, 0xa1d1937e, 0x38d8c2c4, 0x4fdff252,
+  0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
+  0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60,
+  0xdf60efc3, 0xa867df55, 0x316e8eef, 0x4669be79,
+  0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
+  0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f,
+  0xc5ba3bbe, 0xb2bd0b28, 0x2bb45a92, 0x5cb36a04,
+  0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
+  0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a,
+  0x9c0906a9, 0xeb0e363f, 0x72076785, 0x05005713,
+  0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
+  0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21,
+  0x86d3d2d4, 0xf1d4e242, 0x68ddb3f8, 0x1fda836e,
+  0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
+  0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c,
+  0x8f659eff, 0xf862ae69, 0x616bffd3, 0x166ccf45,
+  0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
+  0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db,
+  0xaed16a4a, 0xd9d65adc, 0x40df0b66, 0x37d83bf0,
+  0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
+  0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6,
+  0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
+  0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
+  0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
+)
+
+def cmemcache_hash(key):
+    r"""Calculate a cmemcache-style CRC32 hash of *key*.
+
+    Note that unlike cmemcache's version, this does calculate the key even if
+    only one server exists, mostly because we don't layer violate enough to
+    know how many servers there are or aren't.
+
+    >>> cmemcache_hash("Hello world")
+    3030
+    >>> cmemcache_hash("Hello worle")
+    31953
+    >>> cmemcache_hash("")
+    1
+    """
+    crc = ~0
+
+    for c in key:
+        crc = ((crc & 0xffffffff) >> 8) ^ crc32tab[(crc ^ ord(c)) & 0xff]
+
+    crc = int((~crc >> 16) & 0x7fff)
+
+    return crc or 1
+
+_oldServerHashFunction = None
+
+def cmemcache_activate():
+    """Activate the cmemcache-style CRC32 hasher in python-memcached."""
+    global _oldServerHashFunction
+    serverHashFunction, _oldServerHashFunction = ( cmemcache_hash,
+            memcache.serverHashFunction )
+
+def cmemcache_deactivate():
+    """Reset the hasher function to the real CRC32 function."""
+    global _oldServerHashFunction
+    serverHashFunction, _oldServerHashFunction = _oldServerHashFunction, None
+#  End of import of cmemcache_hash
+########################################################
 
 # vim: ts=4 sw=4 et :
