0b67fd422e62b1a08bbc33f669d2f0371ba2d2d6
Andrey Gubarev <mylokin@me.com>
2016-02-16 15:13:35 +0300

    extract inline class FakeSocket

--- tests/test_setmulti.py
+++ tests/test_setmulti.py
@@ -7,47 +7,49 @@
 #
 #     https://github.com/linsomniac/python-unittest-skeleton
 
-from __future__ import print_function
+from __future__ import (
+    print_function,
+    absolute_import)
 
 import socket
+import logging
 import unittest
 
 import memcache
 
 DEBUG = False
+logger = logging.getLogger(__file__)
+
+class FakeSocket(object):
+    def __init__(self, *args):
+        if DEBUG:
+            logger.debug('FakeSocket{0!r}'.format(args))
+        self._recv_chunks = [b'chunk1']
+
+    def connect(self, *args):
+        if DEBUG:
+            logger.debug('FakeSocket.connect{0!r}'.format(args))
+
+    def sendall(self, *args):
+        if DEBUG:
+            logger.debug('FakeSocket.sendall{0!r}'.format(args))
+
+    def recv(self, *args):
+        if self._recv_chunks:
+            data = self._recv_chunks.pop(0)
+        else:
+            data = ''
+        if DEBUG:
+            logger.debug('FakeSocket.recv{0!r} -> {1!r}'.format(args, data))
+        return data
+
+    def close(self):
+        if DEBUG:
+            logger.debug('FakeSocket.close()')
 
 
-class test_Memcached_Set_Multi(unittest.TestCase):
+class MemcachedSetMultiTestCase(unittest.TestCase):
     def setUp(self):
-        RECV_CHUNKS = [b'chunk1']
-
-        class FakeSocket(object):
-            def __init__(self, *args):
-                if DEBUG:
-                    print('FakeSocket{0!r}'.format(args))
-                self._recv_chunks = list(RECV_CHUNKS)
-
-            def connect(self, *args):
-                if DEBUG:
-                    print('FakeSocket.connect{0!r}'.format(args))
-
-            def sendall(self, *args):
-                if DEBUG:
-                    print('FakeSocket.sendall{0!r}'.format(args))
-
-            def recv(self, *args):
-                if self._recv_chunks:
-                    data = self._recv_chunks.pop(0)
-                else:
-                    data = ''
-                if DEBUG:
-                    print('FakeSocket.recv{0!r} -> {1!r}'.format(args, data))
-                return data
-
-            def close(self):
-                if DEBUG:
-                    print('FakeSocket.close()')
-
         self.old_socket = socket.socket
         socket.socket = FakeSocket
 
@@ -63,7 +65,7 @@ class test_Memcached_Set_Multi(unittest.TestCase):
         self.assertEqual(sorted(bad_keys), ['bar', 'foo'])
 
         if DEBUG:
-            print('set_multi({0!r}) -> {1!r}'.format(mapping, bad_keys))
+            logger.debug('set_multi({0!r}) -> {1!r}'.format(mapping, bad_keys))
 
 
 if __name__ == '__main__':
