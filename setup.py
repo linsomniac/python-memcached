0f0cb639ad2dd42e00c78301cf39f34b5869ec8b
Tim Graham <timograham@gmail.com>
2016-12-14 08:36:29 -0500

    Add testing for Python 3.5

--- setup.py
+++ setup.py
@@ -31,5 +31,6 @@ setup(name="python-memcached",
           "Programming Language :: Python :: 3",
           "Programming Language :: Python :: 3.2",
           "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4"
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
           ])
