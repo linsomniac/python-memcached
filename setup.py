cea0d7693762d4962b429676cc4fd862edc9c90e
Tim Graham <timograham@gmail.com>
2016-12-21 16:12:48 -0500

    Remove testing for Python 3.3

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(name="python-memcached",
           "Programming Language :: Python :: 2",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           ])
